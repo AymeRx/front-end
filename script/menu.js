68643ae9e431625c39265ac5505deaed8a4a94c4
Aymeric Surre <aymeric.surre@etu.univ-grenoble-alpes.fr>
2022-11-14 14:33:16 +0100

    Affichage de chaques sections sur une meme page

---
+++ script/menu.js
@@ -0,0 +1,36 @@
+// afficher la section dont l'id est passé en paramètre
+function afficherSection(id) {
+    //selectionner toutes le sections de la page
+    var sections = document.getElementsByTagName("section");
+    //pour chaque section
+    for (var i = 0; i < sections.length; i++) {
+        //si l'id de la section est différent de l'id passé en paramètre
+        if (sections[i].id != id) {
+            //cacher la section
+            sections[i].style.display = "none";
+        } else {
+            //afficher la section
+            sections[i].style.display = "block";
+        }
+    }
+}
+
+document.getElementById("lienAfficherAccueil").onclick = function() {
+    afficherSection("Accueil");
+};
+
+document.getElementById("lienAfficherActivite").onclick = function() {
+    afficherSection("Activite");
+};
+
+document.getElementById("lienAfficherAudio").onclick = function() {
+    afficherSection("Audio");
+};
+
+document.getElementById("lienAfficherVideo").onclick = function() {
+    afficherSection("Video");
+};
+
+document.getElementById("lienAfficherContact").onclick = function() {
+    afficherSection("Contact");
+}
