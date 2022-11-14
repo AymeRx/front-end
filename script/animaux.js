87a8e152f365c23a1755cc54f155380277067c75
Aymeric Surre <aymeric.surre@etu.univ-grenoble-alpes.fr>
2022-11-14 15:40:01 +0100

    création de la page dynamique animaux

---
+++ script/animaux.js
@@ -0,0 +1,15 @@
+//création d'un tableau associatif contenant les animaux
+var animal1 = new Array();
+animal1["nom"] = "chat";
+animal1["image"] = "chat.jpg";
+animal1["description"] = "lorem ipsum dolor sit amet";
+
+var chien = new Array();
+chien["nom"] = "chien";
+chien["image"] = "chien.jpg";
+chien["description"] = "lorem ipsum dolor sit amet";
+
+var lapin = new Array();
+lapin["nom"] = "lapin";
+lapin["image"] = "lapin.jpg";
+lapin["description"] = "lorem ipsum dolor sit amet";
